8b815a3ab267284d1de3f14bc2795776693bf848
Stella <148039412+Rizannma@users.noreply.github.com>
2026-03-29 19:18:20 +0800

    railway run app

--- Flask Activity/app.py
+++ Flask Activity/app.py
@@ -105,5 +105,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
